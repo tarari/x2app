91dabac24d6c15d2962dce132a9ae5846dbe6c2e
Antonio Jiménez Márquez <tonijimenez@MacBook-Pro-de-Antonio.local>
2020-12-14 20:01:41 +0100

    Logout

--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -40,7 +40,7 @@
         // funcions post -render
         function logout(){
             session_destroy();
-            header('Location:BASE');
+            header('Location:'.BASE);
         }
         
 
